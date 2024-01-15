76e9ff388a411f7b2158925c1508c2678af3c81a
EzCraft-ID <110847895+Ezcraft-ID@users.noreply.github.com>
2024-01-15 12:38:48 +0700

    Update script.js

--- assetslogin/script.js
+++ assetslogin/script.js
@@ -7,7 +7,7 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
     // Dalam contoh ini, kita menggunakan username: admin dan password: 12345 sebagai contoh
     if (username === "admin" && password === "admin") {
         alert("Login berhasil!");
-        window.location.href = "/baru/"; // Alihkan ke tampilan utama web
+        window.location.href = "https://www.youtube.com/watch?v=DeIk06jAULc"; // Alihkan ke tampilan utama web
     } else {
         alert("Username atau password salah!");
     }
